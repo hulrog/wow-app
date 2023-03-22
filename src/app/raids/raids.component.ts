c1d492a85c73e2c2b50b41f52eac52941b57f01e
Hulrog <petar99t@gmail.com>
2023-03-22 14:01:31 +0100

    pozvani podaci za leaderboard

--- src/app/raids/raids.component.ts
+++ src/app/raids/raids.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import axios from 'axios';
 
 @Component({
   selector: 'app-raids',
@@ -6,7 +7,57 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./raids.component.scss'],
 })
 export class RaidsComponent implements OnInit {
-  constructor() {}
+  selectedRaid: string;
+  selectedFaction: string;
+  selectedRegion: string;
+  accessToken: string;
+  namespace: string;
+  locale: string;
+  constructor() {
+    this.selectedRaid = '';
+    this.selectedFaction = 'horde';
+    this.selectedRegion = '';
+    this.accessToken = '';
+    this.namespace = '';
+    this.locale = '';
+  }
 
-  ngOnInit() {}
+  async ngOnInit() {
+    const response = await axios.post(
+      'https://us.battle.net/oauth/token',
+      'grant_type=client_credentials',
+      {
+        auth: {
+          username: '1902de7276ee46c396faf2730b4fe886',
+          password: 'hrdP3d9JN1XEQpROXaD8snsywgjQkBWu',
+        },
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+      }
+    );
+    this.accessToken = response.data.access_token;
+  }
+
+  setFaction(faction: string) {
+    this.selectedFaction = faction;
+  }
+
+  setNamespaceAndLocale() {
+    if (this.selectedRegion === 'eu') {
+      this.namespace = 'dynamic-eu';
+      this.locale = 'en_EU';
+    } else if (this.selectedRegion === 'us') {
+      this.namespace = 'dynamic-us';
+      this.locale = 'en_US';
+    }
+  }
+
+  async getRaidLeaderboard() {
+    const url = `https://${this.selectedRegion}.api.blizzard.com/data/wow/leaderboard/hall-of-fame/${this.selectedRaid}/${this.selectedFaction}?namespace=${this.namespace}&locale=${this.locale}&access_token=${this.accessToken}`;
+    const response = await axios.get(url);
+
+    console.log(response.data);
+    // TODO obraditi response i prikazati na frontu leaderboard
+  }
 }
